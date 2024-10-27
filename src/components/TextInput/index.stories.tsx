8c2f06bb19e6d10b8f5dc301f0a7f646aa45e334
dokyung-kim <dkkim0122@gmail.com>
2024-10-27 18:07:04 +0900

    feat: TextInput 컴포넌트 추가

---
+++ src/components/TextInput/index.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import TextInput from './index'
+
+const meta = {
+  title: 'TextInput',
+  component: TextInput,
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ padding: '3em', backgroundColor: 'beige', width: '100%' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options: ['s', 'l'],
+      },
+    },
+    disabled: {
+      control: 'boolean'
+    },
+    iserror: {
+      control: 'boolean'
+    }
+  },
+} satisfies Meta<typeof TextInput>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Small: Story = {
+  args: {
+    size: 's',
+    placeholder: '입력',
+  },
+}
+
+export const Large: Story = {
+  args: {
+    size: 'l',
+    placeholder: '입력',
+  },
+}
