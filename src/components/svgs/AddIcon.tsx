c4dedc5cf1c2c9f50c02b678a4d41fab80b09e27
dokyung-kim <dkkim0122@gmail.com>
2024-11-02 15:11:35 +0900

    feat: svg 아이콘에 currentColor 적용

--- src/components/svgs/AddIcon.tsx
+++ src/components/svgs/AddIcon.tsx
@@ -8,7 +8,7 @@ const SvgAddIcon = (props) => (
   >
     <g clipPath="url(#addIcon_svg__a)">
       <path
-        fill="#202325"
+        fill="currentColor"
         d="M18.333 18.333v-10h3.334v10h10v3.334h-10v10h-3.334v-10h-10v-3.334z"
       />
     </g>
--- src/components/svgs/SubtractIcon.tsx
+++ src/components/svgs/SubtractIcon.tsx
@@ -7,7 +7,7 @@ const SvgSubtractIcon = (props) => (
     {...props}
   >
     <g clipPath="url(#subtractIcon_svg__a)">
-      <path fill="#202325" d="M8.333 18.333v3.334h23.334v-3.334z" />
+      <path fill="currentColor" d="M8.333 18.333v3.334h23.334v-3.334z" />
     </g>
   </svg>
 );
