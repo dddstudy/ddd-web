61f5b39f8773dd768221aabac01f12b409622769
dokyung-kim <dkkim0122@gmail.com>
2024-11-02 15:22:11 +0900

    feat: IconButton에 필요한 tailwind 값 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config: Config = {
       },
       fontSize,
       spacing,
+      boxShadow: {
+        strong: "0px 6px 12px 0px rgba(0, 0, 0, 0.12), 0px 4px 8px 0px rgba(0, 0, 0, 0.08), 0px 0px 4px 0px rgba(0, 0, 0, 0.08)",
+      }
     },
   },
   plugins: [],
