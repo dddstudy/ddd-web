ebcaeb91bb515e7c1259ffc5001c6ceafa65d183
dokyung-kim <dkkim0122@gmail.com>
2024-11-17 17:47:22 +0900

    feat: Navigation 컴포넌트에 필요한 svg 컴포넌트 추가

---
+++ src/components/svgs/MenuIcon.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+const SvgMenuIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <path fill="#202325" d="M3 5h18v2H3zm0 6h18v2H3zm0 6h18v2H3z" />
+  </svg>
+);
+export default SvgMenuIcon;
+
