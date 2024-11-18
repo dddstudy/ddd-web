57f8bc9d7705a59b4ec2e65826cc0c65730977a7
dokyung-kim <dkkim0122@gmail.com>
2024-11-19 00:00:51 +0900

    refactor: navigation에 조건부 컴포넌트 렌더링 구문 early return으로 변경하여 가독성 및 확장성 높임

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -6,5 +6,9 @@ import MobileNavigation from "./MobileNavigation";
 export default function Navigation() {
   const isDesktop = useMediaQuery("(min-width: 768px)");
 
-  return <>{isDesktop ? <DesktopNavigation /> : <MobileNavigation />}</>;
+  if (isDesktop) {
+    return <DesktopNavigation />;
+  }
+
+  return <MobileNavigation />;
 }
