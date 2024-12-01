66f4e6674605c5cabc478b20f1a8aa421cbad005
dokyung-kim <dkkim0122@gmail.com>
2024-12-01 11:32:45 +0900

    feat: 디자인 수정으로 인해 Navigation의 배경에 backdrop blur 적용

--- src/components/Navigation/DesktopNavigation.tsx
+++ src/components/Navigation/DesktopNavigation.tsx
@@ -12,7 +12,7 @@ export default function DesktopNavigation() {
       <div className="bg-black w-[55px] h-[55px] rounded-full flex justify-center items-center flex-shrink-0">
         <DddIcon />
       </div>
-      <nav className="bg-white p-4 rounded-full flex gap-[2px]">
+      <nav className="bg-white bg-opacity-[.84] p-4 rounded-full flex gap-[2px] backdrop-blur-[2.5px]">
         {navigationList.map((item) => (
           <Link className="flex-shrink-0" href={item.href} key={item.name}>
             <TextButton
