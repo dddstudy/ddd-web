0359c1da83aef2a1d7d8a99a678c46bdd7655901
dokyung-kim <dkkim0122@gmail.com>
2024-12-03 00:09:54 +0900

    fix: 데스크톱에서 새로고침 시 모바일 네비게이션이 나오다 데스크톱 네비게이션으로 바뀌는 현상 해결

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,7 +1,28 @@
 import { useEffect, useState } from "react";
 
+/**
+ * 현재 화면 너비가 인자로 받은 media query string을 만족하는지 여부를 판단하는 커스텀 훅
+ * @param {string} query - media query string, ex. "(max-width: 768px)"
+ * @returns {boolean | null} 컴포넌트가 마운트되기 전에는 null, 마운트된 후에는 화면 너비가 query 조건에 부합하는지 여부
+ * @example
+ * ```
+ * export default function Example() {
+ *  const isDesktop = useMediaQuery("(min-width: 768px)");
+ *    // 컴포넌트 마운트되기 전
+ *    if (isDesktop === null) {
+ *      return null;
+ *    }
+ *  
+ *    if (isDesktop) {
+ *      return <Desktop />;
+ *    }
+ *  
+ *    return <Mobile />;
+ *  }
+ * ```
+ */
 export default function useMediaQuery(query: string) {
-  const [matches, setMatches] = useState(false);
+  const [matches, setMatches] = useState<boolean | null>(null);
 
   function handleChange(e: MediaQueryListEvent) {
     setMatches(e.matches);
