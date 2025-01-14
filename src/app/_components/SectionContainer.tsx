1b02db30b17f8b9facb2322c605749ecd3f71d84
dokyung-kim <dkkim0122@gmail.com>
2025-01-14 22:45:01 +0900

    feat: 메인 페이지에서 사용되는 각 섹션 컨테이너 컴포넌트화

---
+++ src/app/_components/SectionContainer.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/utils/cn";
+
+interface Props {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function SectionContainer({ className, children }: Props) {
+  return (
+    <section
+      className={cn("desktop:px-0 netbook:px-80 tablet:px-32 px-24", className)}
+    >
+      {children}
+    </section>
+  );
+}
