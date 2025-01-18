10f6e6b6d3bba1b56c303674208e999e1dcd2b12
dokyung-kim <dkkim0122@gmail.com>
2025-01-18 23:02:10 +0900

    refactor: sns 목록 공통화

--- src/components/Navigation/MobileNavigation.tsx
+++ src/components/Navigation/MobileNavigation.tsx
@@ -1,32 +1,8 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import {
-  DddIcon,
-  MenuIcon,
-  CloseIcon,
-  InstagramIcon,
-  LinkedInIcon,
-  GithubIcon,
-} from "@/components/svgs";
+import { DddIcon, MenuIcon, CloseIcon } from "@/components/svgs";
+import snsList from "@/components/common/snsList";
 import navigationList from "@/fixtures/navigationList.json";
-
-const snsList = [
-  {
-    name: "Instagram",
-    icon: <InstagramIcon />,
-    link: "https://www.instagram.com/dynamic_ddd?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-  },
-  {
-    name: "LinkedIn",
-    icon: <LinkedInIcon />,
-    link: "https://www.linkedin.com/company/dddcommunity",
-  },
-  {
-    name: "Github",
-    icon: <GithubIcon />,
-    link: "https://github.com/DDD-Community",
-  },
-];
 
 export default function MobileNavigation() {
   const [isMenuOpened, setMenuOpened] = useState(false);
