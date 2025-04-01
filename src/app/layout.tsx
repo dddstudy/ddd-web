890879b969953dbbecfdee7702e25a21dcc3696c
dokyung-kim <dkkim0122@gmail.com>
2025-04-01 23:03:26 +0900

    feat: title과 description 등 메타데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,22 @@ import NotificationRegisterPopup from "@/components/Notification/Popup";
 import MockProviders from "@/mocks/provider";
 
 export const metadata: Metadata = {
-  title: "DDD Website",
-  description: "DDD 웹사이트입니다",
+  title: "DDD - Dynamic Developer Designer IT 연합동아리",
+  description:
+    "개발자, 디자이너, PM이 모두 모여 함께 성장하는 DDD, 하나의 뜻깊은 IT 서비스를 함께 만들어 세상에 선보여요!",
+  keywords: [
+    "IT 동아리",
+    "IT 연합 동아리",
+    "사이드프로젝트",
+    "개발자사이드프로젝트",
+    "앱개발프로젝트",
+    "개발동아리",
+    "디자이너포트폴리오",
+    "앱프로젝트",
+    "직장인IT동아리",
+    "대학생IT동아리",
+    "사이드프로젝트추천",
+  ],
 };
 
 export default function RootLayout({
