e49bc8cf3b8469bf656ba32bbd891a9f84327876
Gn0lee <jinho605@gmail.com>
2025-04-03 00:06:38 +0900

    프로젝트 리스트 스타일 수정

--- src/app/project/_components/ProjectList.tsx
+++ src/app/project/_components/ProjectList.tsx
@@ -7,7 +7,7 @@ interface ProjectListProps {
 
 export default function ProjectList({ projects }: ProjectListProps) {
   return (
-    <div className="grid mobile:grid-cols-1 tablet:grid-cols-2 netbook:grid-cols-3 desktop:grid-cols-3 mobile:gap-y-[48px] tablet:gap-y-[88px] netbook:gap-y-[100px] desktop:gap-y-[100px] desktop:gap-x-[90px] netbook:gap-x-[72px] tablet:gap-x-[72px] mobile:gap-x-[0px]">
+    <div className="grid mobile:grid-cols-1 tablet:grid-cols-2 netbook:grid-cols-3 desktop:grid-cols-3 mobile:gap-y-[48px] tablet:gap-y-[88px] netbook:gap-y-[100px] desktop:gap-y-[100px] desktop:gap-x-[90px] netbook:gap-x-[72px] tablet:gap-x-[72px] mobile:gap-x-[0px]  desktop:max-w-[1200px]">
       {projects.map((project, index) => (
         <ProjectItem key={project.id} project={project} index={index} />
       ))}
---
+++ src/app/project/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useState } from "react";
+import ProjectList from "@/app/project/_components/ProjectList";
+import TabList from "@/app/project/_components/TabList";
+import projects from "@/app/project/data/project";
+
+export default function Project() {
+  const [selectedAppType, setSelectedAppType] = useState<
+    "mobile" | "web" | "all"
+  >("all");
+
+  return (
+    <div className="desktop:px-[24px] netbook:px-[24px] tablet:px-[24px] mobile:px-[24px] pb-[48px] bg-[#EAEAEA]">
+      <TabList
+        all={{
+          tab: {
+            label: "All",
+            value: projects.length,
+            active: selectedAppType === "all",
+          },
+          onClick: () => setSelectedAppType("all"),
+        }}
+        mobile={{
+          tab: {
+            label: "Mobile",
+            value: projects.filter(
+              (project) =>
+                project.typeofApp === "Android" || project.typeofApp === "iOS"
+            ).length,
+            active: selectedAppType === "mobile",
+          },
+          onClick: () => setSelectedAppType("mobile"),
+        }}
+        web={{
+          tab: {
+            label: "Web",
+            value: projects.filter((project) => project.typeofApp === "web")
+              .length,
+            active: selectedAppType === "web",
+          },
+          onClick: () => setSelectedAppType("web"),
+        }}
+      />
+      <div className="flex justify-center">
+        <ProjectList
+          projects={projects.filter((project) => {
+            const filterMap = {
+              all: () => true,
+              mobile: () => ["Android", "iOS"].includes(project.typeofApp),
+              web: () => project.typeofApp === "web",
+            };
+            return filterMap[selectedAppType]();
+          })}
+        />
+      </div>
+    </div>
+  );
+}
