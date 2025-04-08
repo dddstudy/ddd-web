aaa8cf922a3c6577a6cbc1aef64c0ea07c4819b4
dokyung-kim <dkkim0122@gmail.com>
2025-04-08 23:24:30 +0900

    feat: Home 페이지에서 12기 모집 컨텐츠를 보여주도록 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+import { useState } from "react";
 import HomeContainer from "@/app/_components/Home";
+import RecruitContainer from "./_components/Recruit";
 
 export default function Home() {
+  const [homePageMode] = useState<"home" | "recruit">("recruit");
   return (
-    <>
-      <HomeContainer />
-    </>
+    <>{homePageMode === "home" ? <HomeContainer /> : <RecruitContainer />}</>
   );
 }
